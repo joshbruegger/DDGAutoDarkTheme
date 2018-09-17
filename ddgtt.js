20ea9eb41f5e05777824e442ab510dc2a0fa68a5
josh0196 <josh@bruegger.it>
2018-09-17 09:38:26 -0500

    1.1 - Performance improvement
    * improved performance
    Moved some operations from ddgtt.js to options.js to make content script run faster

--- ddgtt.js
+++ ddgtt.js
@@ -1,43 +1,59 @@
+//xpath references
+var darkTheme = getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[4]"); //dark theme button
+var lightTheme = [getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[1]"),
+				  				getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[2]"),
+				  				getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[3]")]; //light themes buttons
+
+
+//setting variables
+var s_startH = 20; //hour from which dark theme is turned on
+var s_endH = 7; //hour from which dark theme is turned on
+var s_lightThemeIdx = 0; //selected light theme. 0=standard  1=dark standard  2=Google style
+var s_cooldownLenght = 1; //cooldown length in hours
+
+//update settings then update theme
+browser.storage.local.get()
+  .then(UpdateSettings, onError);
+
+//--FUNCTIONS--
+
+/// gets element from xpath
 function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-//xpath references
-var darkTheme = getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[4]");
-var lightTheme = [getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[1]"),
-				  getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[2]"),
-				  getElementByXpath("/html/body/div[2]/div[6]/ul/ul[1]/li[2]/ul/li[3]")];
-				  
-//setting variables
-var s_startH = 20;
-var s_endH = 7;
-var s_lightThemeIdx = 0;
+/// called if any errors occur
+function onError(error) {
+  console.log(error)
+}
 
-function updateSettings(item) {
-	
-	if (item.startH) {
-		var startH = item.startH.split(':');
-		var startM = startH[1]!=0? startH[1]/60 : 0;
-		s_startH = Number(startH[0]) + Number(startM);
-	}
+/// gets settings stored locally, if existent, then runs UpdateTheme()
+function UpdateSettings(item) {
 
-	if (item.endH){
-		var endH = item.endH.split(':');
-		var endM = endH[1]!=0? endH[1]/60 : 0;
-		s_endH = Number(endH[0]) + Number(endM);
-	}
-	
-	if (item.selectedTheme)
+	if (item.startH) //!=null
+		s_startH = item.startH;
+
+	if (item.endH) //!=null
+		s_endH = item.endH;
+
+	if (item.selectedTheme) //!=null
 		s_lightThemeIdx = item.selectedTheme-1;
-	
-	//alert("startH: " + s_startH + " endH: " + s_endH + " theme= " + s_lightThemeIdx);
-	UpdateTheme();
+
+	//if (item.cooldownLenght) //!=null
+		//s_cooldownLenght = item.cooldownLenght;
+
+	//if (!item.lastCooldown) // =null
+		//browser.storage.local.set({lastCooldown: 0});
+
+	//if (Date.now() - item.lastCooldown >= s_cooldownLenght*3600000)
+		UpdateTheme();
 }
 
 function UpdateTheme(){
+
 	var d = new Date();
 	var time = d.getHours() + d.getMinutes()/60;
-	
+
 	if (time >= s_startH || time <=s_endH){
 		if (darkTheme.className != "nav-menu__theme  js-side-menu-theme theme-is-selected") {
 			darkTheme.click();
@@ -48,14 +64,6 @@ function UpdateTheme(){
 			lightTheme[s_lightThemeIdx].click();
 		}
 	}
+
+	//utilize existent DDG's "theme changed" snackbar for cooldown feature?
 }
-
-function onError(error) {
-  console.log(error)
-}
-
-//update settings...
-browser.storage.local.get()
-  .then(updateSettings, onError);
-
-//TODO: toast/snackbar to disable extension for x amount of time
--- options.js
+++ options.js
@@ -1,15 +1,24 @@
 function saveOptions(e) {
   e.preventDefault();
   
+  //get theme selected
   var data = new FormData(document.querySelector("form"))
   var selectedTheme = 1;
   for (const entry of data) {
     selectedTheme = entry[1];
   };
   
-  var startH = document.getElementById("startH").value;
-  var endH = document.getElementById("endH").value
+  //get selected start time, convert from hh:mm to hours
+  var tmp_startH = document.getElementById("startH").value.split(':');
+  startM = tmp_startH[1]!=0? tmp_startH[1]/60 : 0;
+  startH = Number(tmp_startH[0]) + Number(startM);
   
+  //get selected end time, convert from hh:mm to hours
+  var tmp_endH = document.getElementById("endH").value.split(':');
+  endM = tmp_endH[1]!=0? tmp_endH[1]/60 : 0;
+  endH = Number(tmp_endH[0]) + Number(endM);
+  
+  //save settings locally
   browser.storage.local.set({startH, endH, selectedTheme});
   
   //debug
@@ -29,11 +38,11 @@ function onError(error) {
 function restoreOptions() {
 	
 	function setChosen (item) {
-		document.getElementById("startH").value = item.startH;
-		document.getElementById("endH").value = item.endH;
+		document.getElementById("startH").value = ('0' + Math.floor(item.startH)).slice(-2) + ":" + ('0' + Math.round((item.startH-Math.floor(item.startH))*60)).slice(-2);
+		document.getElementById("endH").value = ('0' + Math.floor(item.endH)).slice(-2) + ":" + ('0' + Math.round((item.endH-Math.floor(item.endH))*60)).slice(-2);
 		document.getElementById("theme"+item.selectedTheme).checked = true;
 		
-		document.querySelector("#log").innerText = "retrived: theme=" + item.selectedTheme + " startH: " + item.startH + " endH: " + item.endH;
+		document.querySelector("#log").innerText = "retrieved: theme=" + item.selectedTheme + " startH: " + item.startH + " endH: " + item.endH;
 	}
 	
 	browser.storage.local.get()
